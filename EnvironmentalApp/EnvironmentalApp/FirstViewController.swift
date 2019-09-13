50e7896aaf91105d6fe4f38b5a1d2ec58ec65a0c
Alyssa Eckels <eckels1073@students.d211.org>
2019-09-13 16:25:16 -0500

    Added base files for app

---
+++ EnvironmentalApp/EnvironmentalApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  EnvironmentalApp
+//
+//  Created by Pirate on 9/13/19.
+//  Copyright © 2019 Pirate. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
